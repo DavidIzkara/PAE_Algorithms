54716630749ed64c21647b6f26b992f05db80678
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-10-21 12:55:40 +0200

    Implement ShockIndex class to compute Shock Index

---
+++ ShockIndex.py
@@ -0,0 +1,22 @@
+import vitaldb
+
+class ShockIndex:
+    def __init__(self, vf: vitaldb.VitalFile):
+        # Get all available track names in the VitalFile
+        available_tracks = vf.get_track_names()
+
+        # Try to find heart rate and systolic pressure tracks
+        hr_track = next(
+            (t for t in available_tracks if 'Intellivue/ECG_HR' in t),          # First try for ECG_HR
+            next((t for t in available_tracks if 'Intellivue/ABP_HR' in t),     # Then try for ABP_HR
+                 next((t for t in available_tracks if 'Intellivue/HR' in t), None))) # Finally try for generic HR track
+        sys_track = next(
+            (t for t in available_tracks if 'Intellivue/ABP_SYS' in t), # First try for invasive systolic BP
+            next((t for t in available_tracks if 'Intellivue/NIBP_SYS' in t), None)) # Then try for non-invasive systolic BP
+        
+        # Convert the signals to NumPy arrays
+        hr = vf.to_numpy(track_names=hr_track)
+        sys = vf.to_numpy(track_names=sys_track)
+
+        # Computes the Shock Index (heart rate divided by systolic BP)
+        self.values = hr / sys
