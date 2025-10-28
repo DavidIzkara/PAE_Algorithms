a01fc9b8e5c4ab1748a19ae4fce5b0ee7975c624
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-10-28 08:41:42 +0100

    Update to use mean pressure tracks for calculations
    Renaming variables and updating comments

--- systemic_vascular_resistence.py
+++ systemic_vascular_resistence.py
@@ -6,12 +6,12 @@ class SystemicVascularResistance:
         # Get all available track names in the VitalFile
         available_tracks = vf.get_track_names()
 
-        # Try to find systolic pressure tracks
-        sys_track = next(
-            (t for t in available_tracks if 'Intellivue/ABP_MEAN' in t), # First try for invasive systolic BP
-            next((t for t in available_tracks if 'Intellivue/NIBP_MEAN' in t), None)) # Then try for non-invasive systolic BP
+        # Try to find mean pressure tracks
+        mean_track = next(
+            (t for t in available_tracks if 'Intellivue/ABP_MEAN' in t), # First try for invasive mean BP
+            next((t for t in available_tracks if 'Intellivue/NIBP_MEAN' in t), None)) # Then try for non-invasive mean BP
         
-        self.vf = ((vf.to_numpy(track_names = sys_track) - vf.to_numpy(track_names = 'Intellivue/CVP_MEAN')) * 80) / CardiacOutput(vf).values
+        self.vf = ((vf.to_numpy(track_names = mean_track) - vf.to_numpy(track_names = 'Intellivue/CVP_MEAN')) * 80) / CardiacOutput(vf).values
 
 #Calculates Systemic Vascular Resistance using Mean Arterial Pressure, Central Venous Pressure, and Cardiac Output.
 #Handles multiple possible mean arterial pressure track names for robustness.
