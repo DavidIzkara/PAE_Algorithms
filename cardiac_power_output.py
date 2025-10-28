50a7e8e6dec80007e9ea1daaa4b51bd61673531a
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-10-28 13:15:46 +0100

    Refactor mean pressure track selection logic and fix value assignation

--- cardiac_power_output.py
+++ cardiac_power_output.py
@@ -9,9 +9,10 @@ class CardiacPowerOutput:
         # Try to find mean pressure tracks
         mean_track = next(
             (t for t in available_tracks if 'Intellivue/ABP_MEAN' in t), # First try for invasive mean BP
-            next((t for t in available_tracks if 'Intellivue/NIBP_MEAN' in t), None)) # Then try for non-invasive mean BP
+            next((t for t in available_tracks if 'Intellivue/BP_MEAN' in t), # Then try for another possible invasive mean BP
+                next((t for t in available_tracks if 'Intellivue/NIBP_MEAN' in t), None))) # Finally try for non-invasive mean BP
         
-        self.vf = (vf.to_numpy(track_names = mean_track) * CardiacOutput(vf).values) / 451.0
+        self.values = (vf.to_numpy(track_names = mean_track) * CardiacOutput(vf).values) / 451.0
 
 #Calculates Cardiac Power Output by multiplying Mean Arterial Pressure by Cardiac Output and dividing by 451.
 #Handles multiple possible mean pressure track names for robustness.
