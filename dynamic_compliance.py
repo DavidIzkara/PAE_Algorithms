6a015ec0a0f72a81bd4b8184000bd32a12314a10
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-10-28 08:17:30 +0100

    Update dynamic_compliance.py file
    Change name of the file and also change the computation from the previous static compliance to the dynamic one (Ramses wants this one)

--- dynamic_compliance.py
+++ dynamic_compliance.py
@@ -1,9 +1,8 @@
 import vitaldb
-from driving_pressure import DrivingPressure
 
-class Compliance:
+class DynamicCompliance:
     def __init__(self, vf: vitaldb.VitalFile): #Falta ver el nombre correcto de la variable de volumen tidal
-        self.values = vf.to_numpy(track_names = 'Intellivue/Tidal_Volume') - DrivingPressure(vf).values
+        self.values = vf.to_numpy(track_names = 'Intellivue/TV_EXP') / (vf.to_numpy(track_names = 'Intellivue/PIP_CMH2O') - vf.to_numpy(track_names = 'Intellivue/PEEP_CMH2O'))
 #Does the compliance calculation by subtracting driving pressure from tidal volume.
 #Does not require any special handling of missing data as this class is only used when we have the data.
 #Does not need to handle multiple possible track names as these are fixed.
