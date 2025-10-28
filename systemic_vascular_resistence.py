a01668430409e28fe0d163137922e7a51af780ea
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-10-28 13:17:44 +0100

    Refactor mean pressure track selection logic and update value assignement

--- systemic_vascular_resistence.py
+++ systemic_vascular_resistence.py
@@ -9,9 +9,10 @@ class SystemicVascularResistance:
         # Try to find mean pressure tracks
         mean_track = next(
             (t for t in available_tracks if 'Intellivue/ABP_MEAN' in t), # First try for invasive mean BP
-            next((t for t in available_tracks if 'Intellivue/NIBP_MEAN' in t), None)) # Then try for non-invasive mean BP
+            next((t for t in available_tracks if 'Intellivue/BP_MEAN' in t), # Then try for another possible invasive mean BP
+                next((t for t in available_tracks if 'Intellivue/NIBP_MEAN' in t), None))) # Finally try for non-invasive mean BP
         
-        self.vf = ((vf.to_numpy(track_names = mean_track) - vf.to_numpy(track_names = 'Intellivue/CVP_MEAN')) * 80) / CardiacOutput(vf).values
+        self.values = ((vf.to_numpy(track_names = mean_track) - vf.to_numpy(track_names = 'Intellivue/CVP_MEAN')) * 80) / CardiacOutput(vf).values
 
 #Calculates Systemic Vascular Resistance using Mean Arterial Pressure, Central Venous Pressure, and Cardiac Output.
 #Handles multiple possible mean arterial pressure track names for robustness.
