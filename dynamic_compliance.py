8d928eb079e97d39c26881f986f5cfdf0d7595e8
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-11-15 00:06:42 +0100

    Update dynamic_compliance.py

--- dynamic_compliance.py
+++ dynamic_compliance.py
@@ -1,8 +1,35 @@
 import vitaldb
+import math
 
 class DynamicCompliance:
-    def __init__(self, vf: vitaldb.VitalFile): #Falta ver el nombre correcto de la variable de volumen tidal
-        self.values = vf.to_numpy(track_names = 'Intellivue/TV_EXP') / (vf.to_numpy(track_names = 'Intellivue/PIP_CMH2O') - vf.to_numpy(track_names = 'Intellivue/PEEP_CMH2O'))
-#Does the compliance calculation by subtracting driving pressure from tidal volume.
+
+    def is_nan(x):
+        try:
+            return math.isnan(x)
+        except Exception:
+            return False 
+        
+    def __init__(self, vf: vitaldb.VitalFile):
+    
+        tv_track='Intellivue/TV_EXP'
+        pip_track='Intellivue/PIP_CMH2O'
+        peep_track='Intellivue/PEEP_CMH2O'
+
+        # Converts the signals to pandas dataframes
+        tv = vf.to_pandas(track_names=tv_track, interval=0, return_timestamp=True)
+        pip = vf.to_pandas(track_names=pip_track, interval=0, return_timestamp=True)
+        peep = vf.to_pandas(track_names=peep_track, interval=0, return_timestamp=True)
+  
+        # Deletes the nan values
+        tv_clean = tv[tv[tv_track].notna()]
+        pip_clean = pip[pip[pip_track].notna()]
+        peep_clean = peep[peep[peep_track].notna()]
+        
+        # Creates a new dataframe with timestamp | tv_value | pip_value | peep_value where the 3 values come from the same timestamp
+        pre_dc= tv_clean.merge(pip_clean, on="Time").merge(peep_clean, on="Time")
+
+        #Creates the DC dataframe: Timestamp | DC_value
+        self.values = {'Timestamp': pre_dc["Time"], 'DC': (pre_dc[tv_track] / (pre_dc[pip_track] - pre_dc[peep_track]))}
+
 #Does not require any special handling of missing data as this class is only used when we have the data.
 #Does not need to handle multiple possible track names as these are fixed.
