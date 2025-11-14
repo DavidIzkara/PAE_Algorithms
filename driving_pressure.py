e990fbc0123270c6071c968d7334250c9cf0366c
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-11-14 23:56:05 +0100

    Update driving_pressure.py

--- driving_pressure.py
+++ driving_pressure.py
@@ -1,8 +1,32 @@
 import vitaldb
+import math
 
 class DrivingPressure:
+
+    def is_nan(x):
+        try:
+            return math.isnan(x)
+        except Exception:
+            return False  
+        
     def __init__(self, vf: vitaldb.VitalFile):
-        self.values = vf.to_numpy(track_names = 'Intellivue/PPLAT_CMH2O') - vf.to_numpy(track_names = 'Intellivue/PEEP_CMH2O')
+
+        pplat_track='Intellivue/PPLAT_CMH2O'
+        peep_track='Intellivue/PEEP_CMH2O'
+        # Converts the signals to pandas dataframes
+        pplat = vf.to_pandas(track_names=pplat_track, interval=0, return_timestamp=True)
+        peep = vf.to_pandas(track_names=peep_track, interval=0, return_timestamp=True)
+
+        
+        # Deletes the nan values
+        pplat_clean = pplat[pplat[pplat_track].notna()]
+        peep_clean = peep[peep[peep_track].notna()]
+        
+        # Creates a new dataframe with timestamp | pplat_value | peep_value where both values come from the same timestamp
+        pre_dp= pplat_clean.merge(peep_clean, on="Time")
+
+        #Creates the DP dataframe: Timestamp | DP_value
+        self.values = {'Timestamp': pre_dp["Time"], 'DP': pre_dp[pplat_track] - pre_dp[peep_track]} 
 #Does the driving pressure calculation by subtracting PEEP from PLAT.
 #Does not require any special handling of missing data as this class is only used when we have the data.
 #Does not need to handle multiple possible track names as these are fixed.
