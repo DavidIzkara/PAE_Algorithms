0710f29515fc9ebe068be2a2b6e85b2447e529b8
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-11-16 12:12:37 +0100

    Update systemic_vascular_resistence.py

--- systemic_vascular_resistence.py
+++ systemic_vascular_resistence.py
@@ -1,5 +1,5 @@
 import vitaldb
-from cardiac_output import CardiacOutput
+from Algoritmos_VF.cardiac_output import CardiacOutput
 
 class SystemicVascularResistance:
     def __init__(self, vf: vitaldb.VitalFile):
@@ -12,7 +12,23 @@ class SystemicVascularResistance:
             next((t for t in available_tracks if 'Intellivue/BP_MEAN' in t), # Then try for another possible invasive mean BP
                 next((t for t in available_tracks if 'Intellivue/NIBP_MEAN' in t), None))) # Finally try for non-invasive mean BP
         
-        self.values = ((vf.to_numpy(track_names = mean_track) - vf.to_numpy(track_names = 'Intellivue/CVP_MEAN')) * 80) / CardiacOutput(vf).values
+
+        # Converts the signals to pandas dataframes
+        mean = vf.to_pandas(track_names=mean_track, interval=0, return_timestamp=True)
+        cvp_track = 'Intellivue/CVP_MEAN'
+        cvp = vf.to_pandas(track_names=cvp_track, interval=0, return_timestamp=True)
+
+        
+        # Deletes the nan values
+        mean_clean = mean[mean[mean_track].notna()]
+        cvp_clean = cvp[cvp[cvp_track].notna()]
+        
+        co = CardiacOutput(vf).values
+        # Creates a new dataframe with timestamp | bld_value | bld_value where both values come from the same timestamp
+        pre_svr= mean_clean.merge(cvp_clean, on="Time").merge(co, on = 'Timestamp')
+
+        #Creates the SVR dataframe: Timestamp | SVR_value
+        self.values = {'Timestamp': pre_svr["Time"], 'SVR': ((pre_svr[mean_track] - pre_svr[cvp_track])*80)/pre_svr['CO']} 
 
 #Calculates Systemic Vascular Resistance using Mean Arterial Pressure, Central Venous Pressure, and Cardiac Output.
 #Handles multiple possible mean arterial pressure track names for robustness.
