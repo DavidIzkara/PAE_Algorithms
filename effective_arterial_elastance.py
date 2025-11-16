eafe3bb9edd2436835fd163d4344b2fb066d322e
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-11-16 21:43:12 +0100

    Update effective_arterial_elastance.py

--- effective_arterial_elastance.py
+++ effective_arterial_elastance.py
@@ -12,5 +12,19 @@ class EffectiveArterialElastance:
                 next((t for t in available_tracks if 'Intellivue/NIBP_SYS' in t), None))) # Finally try for non-invasive systolic BP
         
 
-        #  0.9 × SBP(systolic Blood Presure) / SV (Stroke Volume)
-        self.values = (0.9 * vf.to_numpy(track_names = sys_track)) / vf.to_numpy(track_names = 'Intellivue/VOL_BLD_STROKE')
+        bld_track = 'Intellivue/VOL_BLD_STROKE'
+
+        # Converts the signals to pandas dataframes
+        sys = vf.to_pandas(track_names=sys_track, interval=0, return_timestamp=True)
+        bld = vf.to_pandas(track_names=bld_track, interval=0, return_timestamp=True)
+
+        
+        # Deletes the nan values
+        sys_clean = sys[sys[sys_track].notna()]
+        bld_clean = bld[bld[bld_track].notna()]
+
+        # Creates a new dataframe with timestamp | hr_value | sys_value where both values come from the same timestamp
+        pre_eae= sys_clean.merge(bld_clean, on="Time")
+
+        #Creates the SI dataframe: Timestamp | SI_value
+        self.values = {'Timestamp': pre_eae["Time"], 'EAE': (0.9 * pre_eae[sys_track]) / pre_eae[bld_track]} 
