0f3a3c0e9c140c23aed0bc712febab6203138a57
DavidIzkara <146666600+DavidIzkara@users.noreply.github.com>
2025-11-16 11:50:00 +0100

    Update cardiac_output.py

--- cardiac_output.py
+++ cardiac_output.py
@@ -11,7 +11,21 @@ class CardiacOutput:
             next((t for t in available_tracks if 'Intellivue/ABP_HR' in t),     # Then try for ABP_HR
                  next((t for t in available_tracks if 'Intellivue/HR' in t), None))) # Finally try for generic HR track
         
-        self.values = vf.to_numpy(track_names = 'Intellivue/VOL_BLD_STROKE') * vf.to_numpy(track_names=hr_track)
+        # Converts the signals to pandas dataframes
+        hr = vf.to_pandas(track_names=hr_track, interval=0, return_timestamp=True)
+        bld_track = 'Intellivue/VOL_BLD_STROKE'
+        bld = vf.to_pandas(track_names=bld_track, interval=0, return_timestamp=True)
+
+        
+        # Deletes the nan values
+        hr_clean = hr[hr[hr_track].notna()]
+        bld_clean = bld[bld[bld_track].notna()]
+        
+        # Creates a new dataframe with timestamp | bld_value | bld_value where both values come from the same timestamp
+        pre_co= bld_clean.merge(hr_clean, on="Time")
+
+        #Creates the CO dataframe: Timestamp | CO_value
+        self.values = {'Timestamp': pre_co["Time"], 'CO': pre_co[bld_track] * pre_co[hr_track]} 
 
 #Calculates Cardiac Output by multiplying Stroke Volume by Heart Rate.
 #Handles multiple possible heart rate track names for robustness.
